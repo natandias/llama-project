ef8538415d4b06914108e4b962b62f32d2ee73bc
natandias <natandias@pm.me>
2023-11-20 03:10:30 -0300

    Feat: align footer

--- llama-front/src/app/layout.tsx
+++ llama-front/src/app/layout.tsx
@@ -24,20 +24,22 @@ export default function RootLayout({
           className={`${inter.className} flex min-h-screen flex-col items-center font-poppins bg-white overflow-x-hidden`}
         >
           {children}
-          <footer className="flex justify-start bg-primary w-screen h-fit ml-0 py-10 mt-20">
-            <Image
-              alt="Intelimaker Logo"
-              src="/android-chrome-192x192.png"
-              className="ml-4 rounded-md"
-              width={200}
-              height={200}
-            />
-            <div className="flex flex-row justify-center w-full">
-              <section className="flex flex-col justify-center items-center flex-wrap gap-5 w-72">
-                <h1 className="text-lg">Produto</h1>
-                <h1 className="text-lg">Quem somos</h1>
-                <p className="text-xl mt-10 ">Intellimaker - 2023</p>
-              </section>
+          <footer className="bg-primary w-screen ml-0 py-10 mt-20">
+            <div className="flex flex-wrap justify-center max-w-screen-limit m-auto">
+              <Image
+                alt="Intelimaker Logo"
+                src="/android-chrome-192x192.png"
+                className="ml-4 rounded-md mb-12 md:mb-0"
+                width={200}
+                height={200}
+              />
+              <div className="flex flex-row justify-center max-w-screen-limit m-auto md:mr-0">
+                <section className="flex flex-col justify-center items-center flex-wrap gap-5 w-72">
+                  <h1 className="text-lg">Produto</h1>
+                  <h1 className="text-lg">Quem somos</h1>
+                  <p className="text-xl mt-10 ">Intellimaker - 2023</p>
+                </section>
+              </div>
             </div>
           </footer>
         </body>
