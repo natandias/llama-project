ba1eebc0789e368f856a6189109a141c3fcad6ab
natandias <natandias@pm.me>
2024-05-08 11:43:49 -0300

    Feat: use input from frontend to build site

--- llama-back/llama.py
+++ llama-back/llama.py
@@ -29,82 +29,82 @@ def get_prompt(model, question):
         return f"""<s>[INST] {question} [/INST]"""
 
 
+def summarize_conversation(conversations):
+    chat_llm = LlamaCpp(
+        model_path=config["LLM_CHAT_MODEL_PATH"],
+        temperature=0.1,
+        n_ctx=4000,
+        top_p=0.95,
+        callback_manager=callback_manager,
+        verbose=True,
+        n_gpu_layers=20,
+        # n_batch=256,
+        f16_kv=True,
+        model_kwargs={"repetition_penalty": 1.1},
+        stop=["###"],
+    )
+
+    prompt = ""
+    for conversation in conversations:
+        person_say = conversation.get("prompt")
+        bot_say = conversation.get("response")
+        prompt += f"""
+        Person: {person_say}
+        Bot: {bot_say}
+        """
+
+        prompt_template = PromptTemplate.from_template(
+            """[INST] Based on the following conversation between a person and a bot, extract the requirements specification for the requested software: {conversation} [/INST]"""
+        )
+
+        prompt_formatted = prompt_template.format(
+            conversation=prompt
+        )
+
+        result = chat_llm(prompt_formatted, max_tokens=3048)
+        formatted_software_requirements = result["choices"][0]["text"]
+        return formatted_software_requirements
+
+
 def create_site_code(site_id):
-    #     chat_llm = LlamaCpp(
-    #         model_path=config["LLM_CHAT_MODEL_PATH"],
-    #         temperature=0.1,
-    #         n_ctx=8000,
-    #         top_p=0.95,
-    #         callback_manager=callback_manager,
-    #         verbose=True,
-    #         n_gpu_layers=20,
-    #         # n_batch=256,
-    #         f16_kv=True,
-    #         model_kwargs={"repetition_penalty": 1.1},
-    #         stop=["###"],
-    #     )
 
-    #     conversation_data = find_conversation(site_id)
-
-    #     all_conversations = conversation_data.get("conversations")
-
-    #     prompt = ""
-    #     for conversation in all_conversations:
-    #         person_say = conversation.get("prompt")
-    #         bot_say = conversation.get("response")
-
-    #         prompt += f"""
-    # Person: {person_say}
-    # Bot: {bot_say}
-    # """
-
-    #     prompt_template = PromptTemplate.from_template(
-    #         """[INST] Based on the following conversation between a person and a bot, extract the requirements specification for the requested software: {conversation} [/INST]"""
-    #     )
-
-    #     prompt_formatted = prompt_template.format(
-    #         conversation=prompt
-    #     )
-
-    #     result = chat_llm(prompt_formatted, max_tokens=3048)
-    #     formatted_software_requirements = result["choices"][0]["text"]\
-
-    #     print(formatted_software_requirements)
-
-    formatted_software_requirements = """Requirements Specification:
-1. Site Structure:
-   - The site will consist of the following pages: Home, About Us, Services, Contact, and Photo Gallery.
-2. Home Page:
-   - A brief description of the site.
-   - Highlights of the most important features of the site.
-3. About Us Page:
-   - Information about the company.
-   - Company history.
-   - Mission and vision statement.
-   - Company values.
-4. Services Page:
-   - A list of services provided by the company.
-   - Brief descriptions of each service.
-5. Detailed Information for Each Service:
-   - Pricing information for each service.
-   - Service duration.
-   - Client testimonials or reviews.
-6. Contact Page:
-   - A contact form for users to send messages directly through the site.
-   - Contact information such as phone number, email address, physical address, and social media links.
-7. Photo Gallery Page:
-   - A collection of photos of cars.
-   - Option to view photos in full screen mode.
-   - Filtering option to view photos by car category.
-8. Additional Features:
-   - Integration of a contact form for users to send messages directly through the site.
-   - Integration of a reservation system for services (if applicable).
-   - Option to share photos from the gallery on social media platforms.
-9. Navigation:
-   - When users click on "About Us" in the menu, they will be taken to the About Us page.
-   - When users click on "Services" in the menu, they will be taken to the Services page, which displays the list of services with brief descriptions.
-   - When users click on "Contact" in the menu, they will be taken to the Contact page, which includes a contact form and contact information.
-   - When users click on "Photo Gallery" in the menu, they will be taken to the Photo Gallery page, which includes a collection of photos and options to view photos in full screen mode and filter by car category."""
+    conversation_data = find_conversation(site_id)
+    all_conversations = conversation_data.get("conversations")
+    formatted_software_requirements = summarize_conversation(all_conversations)
+    #     formatted_software_requirements = """Requirements Specification:
+    # 1. Site Structure:
+    #    - The site will consist of the following pages: Home, About Us, Services, Contact, and Photo Gallery.
+    # 2. Home Page:
+    #    - A brief description of the site.
+    #    - Highlights of the most important features of the site.
+    # 3. About Us Page:
+    #    - Information about the company.
+    #    - Company history.
+    #    - Mission and vision statement.
+    #    - Company values.
+    # 4. Services Page:
+    #    - A list of services provided by the company.
+    #    - Brief descriptions of each service.
+    # 5. Detailed Information for Each Service:
+    #    - Pricing information for each service.
+    #    - Service duration.
+    #    - Client testimonials or reviews.
+    # 6. Contact Page:
+    #    - A contact form for users to send messages directly through the site.
+    #    - Contact information such as phone number, email address, physical address, and social media links.
+    # 7. Photo Gallery Page:
+    #    - A collection of photos of cars.
+    #    - Option to view photos in full screen mode.
+    #    - Filtering option to view photos by car category.
+    # 8. Additional Features:
+    #    - Integration of a contact form for users to send messages directly through the site.
+    #    - Integration of a reservation system for services (if applicable).
+    #    - Option to share photos from the gallery on social media platforms.
+    # 9. Navigation:
+    #    - When users click on "About Us" in the menu, they will be taken to the About Us page.
+    #    - When users click on "Services" in the menu, they will be taken to the Services page, which displays the list of services with brief descriptions.
+    #    - When users click on "Contact" in the menu, they will be taken to the Contact page, which includes a contact form and contact information.
+    #    - When users click on "Photo Gallery" in the menu, they will be taken to the Photo Gallery page, which includes a collection of photos and options to view photos in full screen mode and filter by car category."""
 
     code_llm = LlamaCpp(
         model_path=config["LLM_CODE_MODEL_PATH"],
